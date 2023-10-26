85bcfcee9716a2d8cda962483f24a493afdb8feb
allone9425 <allone9425@gmail.com>
2023-10-26 11:25:24 +0900

    어서오조 팀프로젝트

--- reviews.js
+++ reviews.js
@@ -22,15 +22,45 @@ document
 document
   .querySelector(".review_input_box_frame")
   .addEventListener("submit", function (e) {
-    const reviewUser = document.querySelector(".review_input_name").value;
-    const reviewPassword = document.querySelector(".review_input_pw").value;
-    const reviewComments = document.querySelector(".review_input_text").value;
-    const reviewRate = document.querySelector(".star_rating_number").innerText;
+    try {
+      const reviewUser = document.querySelector(".review_input_name").value;
+      const reviewPassword = document.querySelector(".review_input_pw").value;
+      const reviewComments = document.querySelector(".review_input_text").value;
+      const reviewRate = document.querySelector(
+        ".star_rating_number"
+      ).innerText;
 
-    if (slangFilter(reviewComments)) {
+      if (slangFilter(reviewComments)) {
+        throw new Error("비속어가 포함되었습니다. 다시 입력해주세요");
+      }
+      // 리뷰 정보를 JSON.stringify를 이용하여 문자열화.
+      const reviewObject = {
+        user: reviewUser,
+        pw: reviewPassword,
+        comments: reviewComments,
+        rate: reviewRate,
+        like: 0,
+      };
+      const reviewObjectString = JSON.stringify(reviewObject);
+      // // localStorage 중 현재 ID의 영화 리뷰 키를 필터링하는 코드
+      // const thisMovieReviews = Object.keys(localStorage).filter((review) =>
+      //   // 영화 고유한 id가 들어가야함
+      //   review.includes("123")
+      // );
+
+      const reviewId = new Date().getTime() + Math.random();
+      // 필터링한 현재영화 리뷰의 수를 파악하여 댓글에 ID(인덱스)를 주는 과정
+      localStorage.setItem(`${123}_${reviewId}`, reviewObjectString);
+    } catch (err) {
+      alert("error.message");
+      e.preventDefault();
+    }
+  });
+
+/* if (slangFilter(reviewComments)) {
       alert("비속어가 포함되었습니다. 다시 입력해주세요");
       e.preventDefault();
-      return;
+      return; 
     }
     // 리뷰 정보를 JSON.stringify를 이용하여 문자열화.
     const reviewObject = {
@@ -53,7 +83,7 @@ document
       `${123}_${thisMovieReviews.length + 1}`,
       reviewObjectString
     );
-  });
+  });*/
 
 document
   .querySelector(".review_input_box")
@@ -140,7 +170,7 @@ const addCard = () => {
         <p>
           ${reviewData.comments} </p>
           
-          <textarea class="text-modify"></textarea>
+          <textarea class="text-modify" data-key="${CardKey}"></textarea>
       </div>
 
       <div class="card-container__card-check-good">
@@ -151,6 +181,8 @@ const addCard = () => {
         <button class="like-btn" data-key="${CardKey}">좋아요</button>
         <button class="btn_modify" type="button" data-key="${CardKey}">수정</button>
         <button class="btn_delete" data-key="${CardKey}">삭제</button>
+        <button class="btn_confirm" type="button" data-key="${CardKey}">확인</button>
+        <button class="btn_cancel" data-key="${CardKey}">취소</button>
       </div>
     </div>
   </li>
@@ -182,32 +214,60 @@ document.addEventListener("click", function (e) {
 document.querySelectorAll(".btn_delete").forEach((item) => {
   item.addEventListener("click", function () {
     document.querySelector(".check_pw").dataset["key"] = item.dataset["key"];
+    document.querySelector(".check_pw").dataset["mode"] = "delete";
     document.querySelector(".check_pw").style.display = "block";
-    // localStorage.removeItem(item.dataset["key"]);
-    // window.location.reload();
+  });
+});
+document.querySelectorAll(".btn_modify").forEach((item) => {
+  item.addEventListener("click", function () {
+    document.querySelector(".check_pw").dataset["key"] = this.dataset["key"];
+    document.querySelector(".check_pw").dataset["mode"] = "modify";
+
+    document.querySelector(".check_pw").style.display = "block";
   });
 });
 
-// 삭제 이벤트, 비밀번호 비교 후 삭제
+// 비밀번호 비교
 document.querySelector(".check_pw_btn").addEventListener("click", function () {
-  const localStoragePW = JSON.parse(
-    localStorage.getItem(this.parentElement.dataset["key"])
-  )["pw"];
+  const mode = this.parentElement.dataset["mode"];
+  const currentReviewKey = this.parentElement.dataset["key"];
+  const localStoragePW = JSON.parse(localStorage.getItem(currentReviewKey))[
+    "pw"
+  ];
   const userInputPW = document.querySelector(".check_pw_input").value;
-  console.log(localStoragePW);
   if (localStoragePW === userInputPW) {
-    const result = confirm("정말 삭제하시겠습니까?");
-    if (result) {
-      localStorage.removeItem(this.parentElement.dataset["key"]);
-      window.location.reload();
-      return;
+    if (mode === "delete") {
+      if (confirm("정말 삭제하시겠습니까?")) {
+        localStorage.removeItem(currentReviewKey);
+        window.location.reload();
+      }
+    } else if (mode === "modify") {
+      document.querySelector(
+        `.text-modify[data-key="${currentReviewKey}"]`
+      ).style.display = "block";
+      document.querySelector(
+        `.btn_confirm[data-key="${currentReviewKey}"]`
+      ).style.display = "block";
+      document.querySelector(
+        `.btn_cancel[data-key="${currentReviewKey}"]`
+      ).style.display = "block";
+
+      document.querySelector(
+        `.btn_modify[data-key="${currentReviewKey}"]`
+      ).style.display = "none";
+      document.querySelector(
+        `.btn_delete[data-key="${currentReviewKey}"]`
+      ).style.display = "none";
+      this.parentElement.style.display = "none";
     }
   } else {
     alert("비밀번호가 일치하지 않습니다. 다시 입력해주세요");
+    return;
   }
 });
 
-document.querySelectorAll(".btn_modify").forEach((item) => {
+//수정클릭후 암호입력하고 수정다하고 확인버튼
+document.querySelectorAll(".btn_confirm").forEach((item) => {
   item.addEventListener("click", function () {
     const itemTextArea = this.parentElement.parentElement.querySelector(
       ".text-area .text-modify"
@@ -222,9 +282,24 @@ document.querySelectorAll(".btn_modify").forEach((item) => {
     let textModify = itemTextArea.value;
     //textarea에 담은 데이터를 reviewData.text에 저장함
     reviewData.comments = textModify;
-    //reviewData.push("텍스트"); -> 실패 ㅠ
+    localStorage.setItem(key, reviewData);
+    console.log(localStorage.setItem(key, reviewData));
     console.log("reviewData.text는" + reviewData.comments); //첫번째 입력값만 됨
     console.log("reviewData는" + reviewData);
     console.log("item.dataset[key]는" + item.dataset["key"]); //각각 맞게 찍힘
+  });
+});
+
+//취소버튼 누르면 다닫히기
+document.querySelectorAll(".btn_cancel").forEach(function (item) {
+  item.addEventListener("click", function () {
+    //closest https://developer.mozilla.org/ko/docs/Web/API/Element/closest
+    let parentBox = item.closest(".card-container__card");
+    //console.log(parentBox);
+    parentBox.querySelector(".text-modify").style.display = "none";
+    parentBox.querySelector(".btn_confirm").style.display = "none";
+    parentBox.querySelector(".btn_cancel").style.display = "none";
+    parentBox.querySelector(".btn_modify").style.display = "block";
+    parentBox.querySelector(".btn_delete").style.display = "block";
   });
 });
