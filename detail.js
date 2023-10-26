61d2a3c2aaed798fcc9c72799e027d37ceb7502d
kwanghee <rkdgh4427@gmail.com>
2023-10-26 14:22:20 +0900

    상세페이지 줄거리 부분 수정

--- detail.js
+++ detail.js
@@ -20,46 +20,60 @@ await fetch(movieDetailUrl, options)
   .then((data) => {
     // 여기서 영화 상세 정보를 처리합니다.
     let movie_title = data["title"];
+    let movie_original_title = data["original_title"];
     let movie_overview = data["overview"];
-    let movie_rating = data["vote_average"];
+    let movie_rating = data["vote_average"].toFixed(1);
     let movie_poster = data["poster_path"];
     let movie_date = data["release_date"];
-    let movie_genres = data["genres"];
+    let movie_runtime = data["runtime"];
     console.log(data);
 
     // 아래에 제목, 평점 등등 구간에 맞게 데이터 넣기
     let temp_html = `
-    <div class="">
+    <div class="box Flex p_07" style="background-color:beige">
     <img class="w-30" src="https://image.tmdb.org/t/p/original${movie_poster}"/>
-    <div class="">
-      <div class="Flex_c p_07 font40">
-        <h1 class="">영화제목</h1>
-        <h1 class="">${movie_title}</h1>
+    <div class="Flex_c pl_13 font40" style="background-color:skyblue">
+      <div class="" style="background-color:orange">
+        <h1 class=""><string>${movie_title}</string></h1>
+        <h1 class=""><string>${movie_original_title}</string></h1>
       </div>
-      <div class="gap font50">
+      <div class="gap font50 m_40">
         <div>
           <span class="">개봉 :</span>
           <span>${movie_date}</span>
         </div>
         <div>
-          <span class="">장르 :</span>
-          <span>${movie_genres}</span>
-        </div>
-        <div>
-          <span class="">등급 :</span>
-          <span>19세 이상관람가능</span>
-        </div>
-        <div>
           <span class="">평점 :</span>
-          <span>${movie_rating}</span>
+          <span>${movie_rating}점</span>
         </div>
         <div>
-          <span class="">누적관객 :</span>
-          <span></span>
+          <span class="">상영 시간 : </span>
+          <span>${movie_runtime}분</span>
+        </div>
+        <div>
+              <button class="review_btn font25" id="review_btn">
+                리뷰 남기기
+              </button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+    
+    <div class="w-30 Flex_c p_07 m_70 gap"  style="background-color:yellowgreen">
+      <div class="font50">줄거리</div>
+      <div>${movie_overview}</div>
+    </div>
     `;
-  })
-  .catch((err) => console.error(err));
+    document
+      .querySelector(".detailbox")
+      .insertAdjacentHTML("beforeend", temp_html);
+  });
+
+// 탑버튼
+document.querySelector(".btn_top").addEventListener("click", function (event) {
+  event.preventDefault();
+  window.scroll({
+    top: 0,
+    behavior: "smooth",
+  });
+});
